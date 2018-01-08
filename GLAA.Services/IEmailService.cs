fbd21098a8c7c42404dc640bc2d048f5635fbc9e
Doug McDonald <dougajmcdonald@gmail.com>
2018-01-08 15:06:33 +0000

    Add gov.notify email client integration

---
+++ GLAA.Services/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace GLAA.Services
+{
+    public interface IEmailService
+    {
+        bool Send(MailMessage msg);
+    }
+}
