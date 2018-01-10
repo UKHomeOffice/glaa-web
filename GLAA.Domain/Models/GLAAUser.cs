bed241fb5a7b17a620ca2e29826f1e31e422a394
Doug McDonald <dougajmcdonald@gmail.com>
2018-01-10 14:15:44 +0000

    Add LP role

--- GLAA.Domain/Models/GLAAUser.cs
+++ GLAA.Domain/Models/GLAAUser.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GLAA.Domain.Models
 {
