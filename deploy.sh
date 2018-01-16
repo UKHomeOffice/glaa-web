e69440fa4fbf03a749ba84b2c494a201232b451c
Doug McDonald <dougajmcdonald@gmail.com>
2018-01-16 09:04:32 +0000

    update CI to remove kubedb container

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,6 @@ export KUBE_TOKEN=${KUBE_TOKEN_ACP_NOTPROD}
 
 kd --insecure-skip-tls-verify \
   --timeout 10m0s \
-  -f kubedb/deployment.yaml \
-  -f kubedb/ingress.yaml \
-  -f kubedb/service.yaml \
   -f kube/deployment.yaml \
   -f kube/ingress.yaml \
   -f kube/service.yaml
