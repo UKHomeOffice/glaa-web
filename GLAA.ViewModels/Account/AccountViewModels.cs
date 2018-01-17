088dea2513623a02a24671ffb0af673dde6ce240
Doug McDonald <dougajmcdonald@gmail.com>
2018-01-17 16:40:57 +0000

    Remove remember me option, default to ispersistant

--- GLAA.ViewModels/Account/AccountViewModels.cs
+++ GLAA.ViewModels/Account/AccountViewModels.cs
@@ -58,9 +58,6 @@ namespace GLAA.ViewModels.Account
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
-
-        [Display(Name = "Remember me?")]
-        public bool RememberMe { get; set; }
     }
 
     public class ResetPasswordViewModel
