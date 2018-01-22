c08c8dead72e35cd52f7aaf5cc2bac0769aedc97
MorayM <moray.macdonald1@gmail.com>
2018-01-22 14:26:47 +0000

    more debug info from db script

--- kube-db-setup/db_setup.sh
+++ kube-db-setup/db_setup.sh
@@ -1,2 +1,2 @@
-/opt/mssql-tools/bin/sqlcmd -S $DB_SERVER,$DB_PORT -U $DB_USER -P $DB_PASS -i db_setup.sql
+/opt/mssql-tools/bin/sqlcmd -S $DB_SERVER,$DB_PORT -U $DB_USER -P $DB_PASS -i db_setup.sql -r1
 echo "Done DB Setup"
