54d8ac0b7c4d03cf06e5a3507c750e966bd58bd4
MorayM <moray.macdonald1@gmail.com>
2018-01-26 11:04:31 +0000

    Don't include env vars in config

--- GLAA.Web/Startup.cs
+++ GLAA.Web/Startup.cs
@@ -63,7 +63,7 @@ namespace GLAA.Web
                 builder.AddUserSecrets<Startup>();
             }
 
-            builder.AddEnvironmentVariables();
+            //builder.AddEnvironmentVariables();
 
             Configuration = builder.Build();
         }
@@ -148,8 +148,8 @@ namespace GLAA.Web
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile("secrets/appsettings.secrets.json", optional: true)
-                .AddEnvironmentVariables();
+                .AddJsonFile("secrets/appsettings.secrets.json", optional: true);
+                //.AddEnvironmentVariables();
 
             if (env.IsDevelopment())
             {
