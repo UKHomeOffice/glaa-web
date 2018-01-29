95479d06dafc37273993add4876ac8b5d8ed310d
Doug McDonald <dougajmcdonald@gmail.com>
2018-01-29 07:51:14 +0000

    Improve logging to use DI logging factory

--- GLAA.Services/FileUploadService.cs
+++ GLAA.Services/FileUploadService.cs
@@ -1,5 +1,6 @@
 ﻿using Amazon.S3;
 using Amazon.S3.Model;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,51 +13,43 @@ namespace GLAA.Services
     public class FileUploadService : IFileUploadService
     {
         private readonly IAmazonS3 client;
+        private readonly ILogger logger;
         private static string bucketName = Environment.GetEnvironmentVariable("BUCKET_NAME");
 
 
-        public FileUploadService(IAmazonS3 client)
+        public FileUploadService(IAmazonS3 client, ILoggerFactory logger)
         {
             this.client = client;
+            this.logger = logger.CreateLogger("File Upload Log");
+
         }
 
         public async Task UploadFile(FileStream fileStream)
         {
             try
             {
-                try
+                var putRequest = new PutObjectRequest
                 {
-                    var putRequest = new PutObjectRequest
-                    {
-                        BucketName = bucketName,
-                        Key = "test_" + DateTime.Now.ToShortTimeString(),
-                        InputStream = fileStream
-                    };
+                    BucketName = bucketName,
+                    Key = "test_" + DateTime.Now.ToShortTimeString(),
+                    InputStream = fileStream
+                };
 
-                    var response = await client.PutObjectAsync(putRequest);
-                }
-                catch (AmazonS3Exception amazonS3Exception)
-                {
-                    if (amazonS3Exception.ErrorCode != null &&
-                        (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId")
-                        ||
-                        amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
-                    {
-                        Console.WriteLine("Check the provided AWS Credentials.");
-                        Console.WriteLine("For service sign up go to http://aws.amazon.com/s3");
-                    }
-                    else
-                    {
-                        Console.WriteLine(
-                            "Error occurred. Message:'{0}' when writing an object"
-                            , amazonS3Exception.Message);
-                    }
-                }
-                
+                var response = await client.PutObjectAsync(putRequest);
             }
-            catch (Exception e)
+            catch (AmazonS3Exception amazonS3Exception)
             {
-                Debug.WriteLine(e);
+                if (amazonS3Exception.ErrorCode != null &&
+                    (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId")
+                    ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    logger.LogError("File upload failed: Check the provided AWS Credentials.");
+                }
+                else
+                {
+                    logger.LogError("Error occurred. Message:'{0}' when writing an object", amazonS3Exception.Message);
+                }
             }
         }
     }
