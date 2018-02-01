fedc4d6920feaf7493197314e4085a7447fa9b7f
MorayM <moray.macdonald1@gmail.com>
2018-02-01 10:31:52 +0000

    fix deployment deletion commands

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ export KUBE_NAMESPACE=glaa-dev
 export KUBE_SERVER=${KUBE_SERVER_ACP_NOTPROD}
 export KUBE_TOKEN=${KUBE_TOKEN_ACP_NOTPROD}
 
-#kubectl delete deployments glaa-web
-#kubectl delete deployments glaa-db-setup
+kubectl --server=$KUBE_SERVER --token=$KUBE_TOKEN --namespace=$KUBE_NAMESPACE delete deployments glaa-web
+kubectl --server=$KUBE_SERVER --token=$KUBE_TOKEN --namespace=$KUBE_NAMESPACE delete deployments glaa-db-setup
 
 kd --debug --insecure-skip-tls-verify \
   --timeout 10m0s \
