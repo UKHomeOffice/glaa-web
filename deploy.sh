5a7cf96977e83fc49e5c474a3651ffa4c8220302
Doug McDonald <dougajmcdonald@gmail.com>
2018-02-05 09:21:48 +0000

    remove seperate web deployment

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,6 @@ export KUBE_TOKEN=${KUBE_TOKEN_ACP_NOTPROD}
 kd --debug --insecure-skip-tls-verify \
   --timeout 10m0s \
   -f kube-db-setup/deployment.yaml \
-  -f kube/deployment.yaml \
   -f kube/ingress.yaml \
   -f kube/service.yaml \
   -f kube/network-policy.yaml \
