ffa11311ee3c78d515bd87c549ef89b8535fa758
MorayM <moray.macdonald1@gmail.com>
2018-02-05 13:32:46 +0000

    Load user's address manually

--- GLAA.Services/AccountCreation/AccountCreationViewModelBuilder.cs
+++ GLAA.Services/AccountCreation/AccountCreationViewModelBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using AutoMapper;
 using GLAA.Domain.Models;
+using GLAA.Repository;
 using GLAA.ViewModels.LicenceApplication;
 using Microsoft.AspNetCore.Identity;
 
@@ -10,27 +11,31 @@ namespace GLAA.Services.AccountCreation
     {
         private readonly IMapper mapper;
         private readonly UserManager<GLAAUser> userManager;
+        private readonly IEntityFrameworkRepository repository;
 
-        public AccountCreationViewModelBuilder(IMapper mp, UserManager<GLAAUser> um)
+        public AccountCreationViewModelBuilder(IMapper mp, UserManager<GLAAUser> um, IEntityFrameworkRepository repo)
         {
             mapper = mp;
             userManager = um;
+            repository = repo;
         }
 
         public EligibilityViewModel Build(string email)
         {
-            return Build<EligibilityViewModel>(email);
-        }
-
-        public T Build<T>(string email) where T : new()
-        {
             if (string.IsNullOrEmpty(email))
             {
-                return new T();
+                return new EligibilityViewModel();
             }
 
             var user = userManager.FindByEmailAsync(email).GetAwaiter().GetResult();
-            var model = mapper.Map<T>(user);
+            var model = mapper.Map<EligibilityViewModel>(user);
+
+            if (user.AddressId != null)
+            {
+                var adr = repository.GetById<Address>(user.AddressId.Value);
+                model.Address = mapper.Map<AddressViewModel>(adr);
+            }
+
             return model;
         }
     }
--- GLAA.Services/AccountCreation/IAccountCreationViewModelBuilder.cs
+++ GLAA.Services/AccountCreation/IAccountCreationViewModelBuilder.cs
@@ -5,7 +5,5 @@ namespace GLAA.Services.AccountCreation
     public interface IAccountCreationViewModelBuilder
     {
         EligibilityViewModel Build(string email);
-
-        T Build<T>(string email) where T : new();
     }
 }
