829f2e4e6becba9eed27901f720dbef383f1c9b9
Sayer Hilton <sayer@yamamotostudios.com>
2018-02-28 14:03:08 +0000

    Added FileUpload controller, view and model

---
+++ GLAA.ViewModels/FileUploadViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace GLAA.ViewModels
+{
+    public class FileUploadViewModel
+    {
+        public IFormFile FormFileUpload { get; set; }
+    }
+}
---
+++ GLAA.Web/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using GLAA.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GLAA.Web.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //TODO - Get currently staged files for upload.
+
+            return View();
+        }
+
+        [HttpPost]
+        public void Index(FileUploadViewModel fileUploadViewModel)
+        {
+            //TODO - handle file upload.
+
+            RedirectToAction("Index");
+        }
+    }
+}
