3a7b2b72ff9378def6ad1d44a868153667bcf1b3
MorayM <moray.macdonald1@gmail.com>
2018-03-01 09:44:01 +0000

    Add new methods to form definition for calculating form order

--- GLAA.Web/FormLogic/LicenceApplicationFormDefinition.cs
+++ GLAA.Web/FormLogic/LicenceApplicationFormDefinition.cs
@@ -68,7 +68,96 @@ namespace GLAA.Web.FormLogic
 
         public int GetSectionLength(FormSection section)
         {
-            return fieldConfiguration.Fields[section].Length;
+            return fieldConfiguration[section].Length;
+        }
+
+        public FormPageDefinition GetLastPage(FormSection section)
+        {
+            return fieldConfiguration[section].Last();
+        }
+
+        public bool IsNextPageLastPage(FormSection section, string actionName)
+        {
+            return fieldConfiguration[section][GetSectionLength(section) - 2].MatchesName(actionName);
+        }
+
+        public FormPageDefinition GetNextPage(FormSection section, string actionName)
+        {
+            // Get the index of the page that matches this action name
+            for (var i = 0; i < GetSectionLength(section); i++)
+            {
+                if (fieldConfiguration[section][i].MatchesName(actionName))
+                {
+                    // return the next page in the sequence
+                    return fieldConfiguration[section][i + 1];
+                }
+            }
+
+            // if in doubt, return the final (summary) screen
+            return GetLastPage(section);
+        }
+
+        public FormPageDefinition GetPreviousPage(FormSection section, string actionName)
+        {
+            // Counting back from the end, find the page that matches this action name
+            for (var i = GetSectionLength(section) - 1; i > 0; i--)
+            {
+                if (fieldConfiguration[section][i].MatchesName(actionName))
+                {
+                    // return the page before the match
+                    return fieldConfiguration[section][i - 1];
+                }
+            }
+
+            // if in doubt, return the final (summary) screen
+            return GetLastPage(section);
+        }
+
+        public FormPageDefinition GetNextPossibleAction<TParent>(FormSection section, string actionName, TParent parent)
+        {
+            var matchLocation = 0;
+            // Search through the list of pages for this section until we find the current one
+            for (var i = 0; i < GetSectionLength(section) - 1; i++)
+            {
+                // Only start looking for the next possible action once we know where to start looking *from*
+                if (fieldConfiguration[section][i].MatchesName(actionName))
+                {
+                    matchLocation = i;
+                }
+
+                // If we're past the current page, can we view this one?
+                if (i > matchLocation && CanViewPage(section, fieldConfiguration[section][i].ActionName, parent))
+                {
+                    // If so, return it. If not, try the next page.
+                    return fieldConfiguration[section][i];
+                }
+            }
+
+            // if in doubt, return the final (summary) screen
+            return GetLastPage(section);
+        }
+
+        public FormPageDefinition GetPreviousPossibleAction<TParent>(FormSection section, string actionName, TParent parent)
+        {
+            var matchLocation = GetSectionLength(section) - 1;
+            // Search back through the list of pages until we find the current one
+            for (var i = GetSectionLength(section) - 1; i > 0; i--)
+            {
+                // Only start looking for the previous possible action once we know where to start looking from
+                if (fieldConfiguration[section][i].MatchesName(actionName))
+                {
+                    matchLocation = i;
+                }
+                // If we've gone back past the current page, can we view this one?
+                if (matchLocation < i && CanViewPage(section, fieldConfiguration[section][i].ActionName, parent))
+                {
+                    // If so, return it. If not, try the previous page.
+                    return fieldConfiguration[section][i];
+                }
+            }
+
+            // if in doubt, return the final (summary) screen
+            return GetLastPage(section);
         }
 
         [System.Obsolete]
